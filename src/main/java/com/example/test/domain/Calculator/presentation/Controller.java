58271c2d6be98da0c5b996dfd8b01d80f81d49c6
Kanghee <kh63167574@gmail.com>
2025-05-13 22:00:44 +0900

    MyFirst

---
+++ src/main/java/com/example/test/domain/Calculator/presentation/Controller.java
@@ -0,0 +1,29 @@
+package com.example.test.domain.Calculator.presentation;
+
+import com.example.test.domain.Calculator.presentation.dto.Request;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/calculator")
+public class Controller {
+
+    @PostMapping("/plus")
+    public ResponseEntity<Integer> plus(@RequestBody Request request){
+        return ResponseEntity.ok(request.getNum1() + request.getNum2());
+    }
+
+    @PostMapping("/minus")
+    public ResponseEntity<Integer> minus(@RequestBody Request request){
+         return ResponseEntity.ok(request.getNum1() - request.getNum2());
+    }
+    @PostMapping("/multiplication")
+    public ResponseEntity<Integer> multiplication(@RequestBody Request request){
+        return ResponseEntity.ok(request.getNum1() * request.getNum2());
+    }
+    @PostMapping("/division")
+    public ResponseEntity<Integer> division(@RequestBody Request request){}
+}
---
+++ src/main/java/com/example/test/domain/Calculator/presentation/dto/Request.java
@@ -0,0 +1,14 @@
+package com.example.test.domain.Calculator.presentation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Request {
+    private int num1;
+    private int num2;
+}
+
+
